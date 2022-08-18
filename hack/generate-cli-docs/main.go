9472b75cb960bca9af2b919c68fa7cb2e2950142
Piaras Hoban <phoban01@gmail.com>
2022-08-18 22:51:19 +0100

    Add cli docs generation script
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

---
+++ hack/generate-cli-docs/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/open-component-model/ocm/cmds/ocm/app"
+	"github.com/open-component-model/ocm/pkg/contexts/clictx"
+)
+
+func main() {
+	fmt.Println("> Generate Docs for OCM CLI")
+
+	if len(os.Args) != 2 { // expect 2 as the first one is the programm name
+		fmt.Printf("Expected exactly one argument, but got %d", len(os.Args)-1)
+		os.Exit(1)
+	}
+	outputDir := os.Args[1]
+
+	// clear generated
+	check(os.RemoveAll(outputDir))
+	check(os.MkdirAll(outputDir, os.ModePerm))
+
+	cmd := app.NewCliCommand(clictx.DefaultContext())
+	cmd.DisableAutoGenTag = true
+
+	err := GenMarkdownTree(cmd, outputDir)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Successfully written docs to %s\n", outputDir)
+}
+
+func printHelp() {
+	fmt.Print(`
+generate-docs [output-dir]
+`)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ hack/generate-cli-docs/mod.go
@@ -0,0 +1,49 @@
+// Copyright 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package main
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// UseLine puts out the full usage for a given command (including parents).
+func UseLine(c *cobra.Command) string {
+	useline := c.Use
+	if strings.Index(useline, " ") < 0 {
+		// no syntax given
+		if c.HasAvailableLocalFlags() {
+			useline += " [<options>]"
+		}
+		if c.HasAvailableSubCommands() {
+			if c.Runnable() {
+				useline += " [<sub command> ...]"
+			} else {
+				useline += " <sub command> ..."
+			}
+		}
+	}
+	if c.HasParent() {
+		useline = c.Parent().CommandPath() + " " + useline
+	}
+	if c.DisableFlagsInUseLine {
+		return useline
+	}
+	if c.HasAvailableFlags() && !strings.Contains(useline, "[<options>]") {
+		useline += " [<options>]"
+	}
+	return useline
+}
