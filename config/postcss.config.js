2ba14d3cde5a2db4d257a126c947c223f427d4af
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-01-30 18:19:49 +0100

    adopt workflow for OCM CLI ref docs
    On-behalf-of: Gerald Morrison (SAP) <gerald.morrison@sap.com>
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

---
+++ config/postcss.config.js
@@ -0,0 +1,6 @@
+// Minimal PostCSS config to satisfy Hugo's PostCSS transform.
+// Doks and theme assets will work without additional plugins.
+// If needed later, add plugins like 'autoprefixer' here.
+module.exports = {
+  plugins: []
+};
