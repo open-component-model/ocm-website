49747bff0f6d956aa1e4ab3f4b47e147ffd2e2ac
Gerald Morrison (SAP) <gerald.morrison@sap.com>
2026-02-11 20:07:18 +0100

    Revert "convert to helper function"
    This reverts commit db03a411fe2d381d777f92d234c5fabf456ae09e.
    
    Signed-off-by: Gerald Morrison (SAP) <gerald.morrison@sap.com>

--- hack/generate-cli-docs/generate_markdown.go
+++ hack/generate-cli-docs/generate_markdown.go
@@ -124,8 +124,9 @@ func genMarkdownCustom(cmd *cobra.Command, w io.Writer, cmdToLink map[string]str
 
 	buf := new(bytes.Buffer)
 	name := cmd.CommandPath()
-	links := commandLinks(cmdToLink)
-	linkHandler := links.handler
+	linkHandler := func(path string) string {
+		return commandLink(path, cmdToLink)
+	}
 
 	if cmd.Runnable() || cmd.HasAvailableSubCommands() && name != "ocm" {
 		buf.WriteString("### Usage\n\n")
@@ -198,7 +199,7 @@ func genMarkdownCustom(cmd *cobra.Command, w io.Writer, cmdToLink map[string]str
 				subheader = true
 			}
 			cname := name + " " + "<b>" + child.Name() + "</b>"
-			buf.WriteString(fmt.Sprintf("* [%s](%s)\t &mdash; %s\n", cname, linkHandler(child.CommandPath()), child.Short))
+			buf.WriteString(fmt.Sprintf("* [%s](%s)\t &mdash; %s\n", cname, cmdToLink[child.CommandPath()], child.Short))
 		}
 		if subheader {
 			buf.WriteString("\n")
@@ -280,10 +281,7 @@ func commandToDir(command string) string {
 	return strings.TrimPrefix(strings.Replace(command, " ", "-", -1), "ocm-")
 }
 
-// Provides a named handler for command path link lookups.
-type commandLinks map[string]string
-
 // Resolves a command path to its generated markdown link.
-func (links commandLinks) handler(path string) string {
-	return links[path]
+func commandLink(path string, cmdToLink map[string]string) string {
+	return cmdToLink[path]
 }
